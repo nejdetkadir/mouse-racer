963e5e4eac61bb5096ac883c86c2bc8b1e7e6eec
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-14 00:35:55 +0300

    Create connection method in indexFactory

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,3 +1,11 @@
-app.controller('indexController', ['$scope', ($scope) => {
-  const socket = io.connect('http://localhost:3000');
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+  const url = 'http://localhost:3000'; // development
+  indexFactory.connectSocket(url, {
+    reconnectionAttempts: 3,
+    reconnectionDelay: 600
+  }).then((socket) => {
+    //console.log('connection successfully', socket);
+  }).catch((err) => {
+    console.log(err);
+  });
 }]);
